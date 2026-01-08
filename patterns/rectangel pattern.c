55c564f661f16d86803d3888b1fb7e9d395b4f34
mathan patel <manthanpatel966@gmail.com>
2026-01-08 14:34:25 -0800

    Added new files

--- patterns/rectangel pattern.c
+++ patterns/rectangel pattern.c
@@ -1,22 +1,15 @@
 #include<stdio.h>
 #include<conio.h>
-int main(){
+int main() {
 	
 	int i,j;
 	
 	for(i=1;i<=5;i++){
-		for(j=1;j<=i;j++){
-			if(j % 2 == 0){
-				printf("0");
-			}
-			else{
-				printf("1");
-			}
+		for(j=1;j<=5;j++){
+			printf(" *");
 		}
 		printf("\n");
-		
 	}
-	
 	
 	
 	
---
+++ patterns/triangelreverse.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main() {
+	
+	int i,j,row=5;
+	
+	for(i=row;i>=1;i--){
+		for(j=1;j<=row-i;j++){
+			printf(" ");
+		}
+		for(j=1;j<=2*i-1;j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+		
+		
+	
+	
+	
+	return 0;
+}
